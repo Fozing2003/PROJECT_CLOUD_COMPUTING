fe01faeae3e42fbc4a13b1132156d3afbf9e5a98
lisefozing <fozinglise@gmail.com>
2025-11-29 19:04:39 +0100

    modification de la structure du projet

---
+++ backend/src/main/java/com/example/projet_cloud_backend/ProjetCloudBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_cloud_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCloudBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetCloudBackendApplication.class, args);
+	}
+
+}
